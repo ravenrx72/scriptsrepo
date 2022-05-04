9f45313567d2756f9f16e38b8967d47e0c790f72
ravenrx72 <info@raptorit.com>
2022-05-03 19:28:30 -0500

    simple tcp client

---
+++ clients/tcp.py
@@ -0,0 +1,20 @@
+#!/bin/python
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+#create socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#create the client
+client.connect((target_host,target_port))
+
+#send some info
+client.send(b"GET / HTTP/1.1\r\n\nHost: google.com\r\n\r\n")
+
+#receive
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
