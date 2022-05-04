2beae67ad7179737a51defeb8f90f9d4be143117
ravenrx72 <info@raptorit.com>
2022-05-03 20:04:25 -0500

    server update

--- clients/server-tcp.py
+++ clients/server-tcp.py
@@ -16,4 +16,16 @@ def main():
 
     while True:
         client, address = server.accept()
+        print(f'[*] Accepted connection from {address[0]}: {address[1]}')
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
+
+
+def handle_client(client_socket):
+    with client_socket as sock:
+        request = sock.recv(1024)
+        print(f'[*] Received: {request.decode("utf-8")}')
+        sock.send(b'ACK')
         
+if _name_ == '_main_':
+    main()
